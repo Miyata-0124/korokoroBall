1a9e292483a73a60327f22dec0ee75fa2da1f715
MiyataKouki <k021g1124@g.neec.ac.jp>
2022-05-24 18:34:14 +0900

    SE追加+確認課題ゲームレベルからタイトルまで

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+    public GameObject clearText; //レベルクリアと表示するテキストを格納
+    public GameObject nextButton;//次のレベルへ偏移するボタンを格納
+    public AudioSource audioSource;//音楽再生するコンポーネント
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("ゴール!");
+
+        clearText.SetActive(true);//無効になっていたオブジェクトをこのタイミングで有効にする
+        nextButton.SetActive(true);
+        audioSource.Play();//Playメソッドの実行で音を再生可能
     }
     // Start is called before the first frame update
     void Start()
